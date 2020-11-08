f512c1d446764e2b23936c2c3b765d8f146563ab
Nazeeha Harun <nazeeha.harun@gmail.com>
2020-11-07 20:30:37 -0500

    db

--- db/server.js
+++ db/server.js
@@ -14,6 +14,8 @@ const moviesRoute = require("../api/handlers/movies");
 const usersRoute = require("../api/handlers/users");
 const peopleRoute = require("../api/handlers/people");
 
+
+
 app.use('/movies', moviesRoute);
 app.use('/users', usersRoute);
 app.use('/people', peopleRoute);
