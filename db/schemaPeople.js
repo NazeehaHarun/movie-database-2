5ece42f60c2b4d23856744e96d0007408614e961
Nazeeha Harun <nazeeha.harun@gmail.com>
2020-11-21 11:17:07 -0500

    database

--- db/schemaPeople.js
+++ db/schemaPeople.js
@@ -1,20 +1,20 @@
 const mongoose = require("mongoose")
 
 let peopleSchema = mongoose.Schema({
-    Role: String,
-    Name: String,
-    id:String,
-    Description:String,
-    C1:String,
-    C2:String,
-    C3:String,
-    C4:String,
-    C5:String,
-    C6:String,
-    Profile:String,
-    M1:String,
-    M2:String,
-    M3:String
+    Role: {type: String, required:true},
+    Name: {type: String, required:true},
+    id:{type: String, required:true},
+    Description:{type: String, required:true},
+    C1:{type: String, required:true},
+    C2:{type: String, required:true},
+    C3:{type: String, required:true},
+    C4:{type: String, required:true},
+    C5:{type: String, required:true},
+    C6:{type: String, required:true},
+    Profile:{type: String, required:true},
+    M1:{type: String, required:true},
+    M2:{type: String, required:true},
+    M3:{type: String, required:true}
 });
 
 module.exports = mongoose.model('People', peopleSchema);
