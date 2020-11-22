ff70795694c5e6874c72287e0c8ac6af283fb1e9
Dioceus <chuwei.chen2001@gmail.com>
2020-11-22 01:21:54 -0500

    Added bcrypt to authentication

--- api/functions/login.js
+++ api/functions/login.js
@@ -2,16 +2,11 @@ const users = require("../../db/users-data.json");
 
 const loginUser = (userObj) => {
     
-    let currentUser = null;
-
-    users.forEach(user => {
-        if (user.userName === userObj.username && user.password === userObj.password) {
-            currentUser = user;
-            
-        }
-    });
-
-    return currentUser;
+    if (!userObj.username || !userObj.password) {
+        return null;
+    } else {
+        return userObj;
+    }
 
 }
 
